57a21049fc8611798871d4271c0eb0a5b85dc00a
shirin faile <shirin.faile@hotmail.com>
2024-12-17 14:31:38 +0100

    end day

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import bcrypt from 'bcrypt';
 import User from '../models/user';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
@@ -35,6 +36,40 @@ router.post('/register', async (req: Request, res: Response) => {
         res.status(201).json({ message: 'User registered successfully' });
     }catch (error) {
         res.status(500).json({ message: 'Server error' });
+    }
+});
+
+// Login an existing user
+router.post('/login', async (req: Request, res: Response) => {
+    try {
+        const { username, password } = req.body;
+
+// Check if the user exists
+        const user = await User.findOne({ username });
+        if (!user) {
+            return res.status(400).json({ message: 'Invalid credentials' });
+        }
+
+// Compare the password
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Invalid credentials' });
+        }
+
+// Generate a JWT token
+        const token = jwt.sign(
+            { id: user._id },
+            'yourSecretKey',
+            { expiresIn: '1hr'}
+        );
+
+        res.json({
+            message: 'Login successful',
+            token,
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server error'});
     }
 });
 
